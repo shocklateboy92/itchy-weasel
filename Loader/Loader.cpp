d0eb81a9bf263fdbc02f11ec2eeeee4b83b01e5a
Lasath Fernando <devel@lasath.org>
2015-08-13 16:05:26 +1000

    Make Loader use its precompiled header

--- Loader/Loader.cpp
+++ Loader/Loader.cpp
@@ -2,15 +2,15 @@
 //
 
 #include "stdafx.h"
-
-#include <CvGameCoreDLLPCH.h>
-#include <CvDllContext.h>
+#include <iostream>
 
 extern "C" ICvGameContext1* DllGetGameContext();
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	ICvGameContext1 *context = DllGetGameContext();
+
+	std::cerr << "Holy S*#T we have a GameContext!" << std::endl;
 	
 	return 0;
 }
