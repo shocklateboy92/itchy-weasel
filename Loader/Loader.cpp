f934d18fde8324dfdd5141dfac1feaf96f42a860
Lasath Fernando <devel@lasath.org>
2015-08-13 13:13:05 +1000

    Start creating DLL loader
    This thing will hopefully replace the game engine one day.

---
+++ Loader/Loader.cpp
@@ -0,0 +1,15 @@
+// Loader.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <CvGameCoreDLLPCH.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	ICvGame1 *game = nullptr;
+	
+	return 0;
+}
+
+
