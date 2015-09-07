3cf0fb4f103103b6d32cc69e3b60690abe56a7c6
Lasath Fernando <devel@lasath.org>
2015-09-07 17:31:39 +1000

    Get script iterating through ICvGameContext methods

--- scripts/debug2.js
+++ scripts/debug2.js
@@ -12,6 +12,44 @@ function bind() {
 	console.log("Found Module: " + cvModule.name);
 
 	threadProbe();
+	gameStateBind();
+}
+
+function gameStateBind() {
+	var entryPoint = Module.findExportByName(cvModule.name, "DllGetGameContext");
+	var getGameState = new NativeFunction(entryPoint, 'pointer', []);
+	var gameState = getGameState();
+
+	var vtable = Memory.readPointer(gameState);
+	console.log("Found ICvGameContext1 at " + vtable);
+
+	for (var i = 0; i < 65; i++) {
+		var fn = Memory.readPointer(vtable.add(Process.pointerSize * i));
+		console.log("\tFound method: " + DebugSymbol.fromAddress(fn).name + "()");
+	}
+}
+
+// this is probably not worth it, since the DLL only exports 1 entry point.
+function exportBind() {
+	console.log("Binding to all exports...");
+	Module.enumerateExports(cvModule.name, {
+		onMatch: function(exp) {
+			if (exp.type === "function") {
+				console.log(exp.name + ": " + exp.address);
+				Interceptor.attach(exp.address, {
+					onEnter: function(args) {
+						console.log("Entering function " + exp.name + "()");
+					},
+					onLeave: function(retval) {
+						console.log(exp.name + ": retval = " + retval);
+					}
+				});
+			}
+		},
+		onComplete: function() {
+			console.log("Export binding complete.");
+		}
+	});
 }
 
 function threadProbe() {
